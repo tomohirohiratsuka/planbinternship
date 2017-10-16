d93dd952e158aad71fadea84f0f78ad53a6924fd
tomohirohiratsuka <tomohiro.hiratsuka@plan-b.co.jp>
2017-10-16 00:26:23 -0700

    ドリル6問目

---
+++ phpdrill1-6.php
@@ -0,0 +1,16 @@
+<?php
+$ary = array("shoebill","lungfish","gibbon");
+$ary2 = array(5,6,7,8,9);
+$ary3 = array();
+foreach ($ary2 as $value2){
+$value2 = $value2 + 10 ;
+echo $value2 . "\n";
+}
+//foreach ($ary as $value) {
+//echo $value . "\n";
+//}
+
+
+
+
+?>
