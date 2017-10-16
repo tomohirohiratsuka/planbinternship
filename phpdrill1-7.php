02235ffda4857e91b269a7f440b782768bc24b71
tomohirohiratsuka <tomohiro.hiratsuka@plan-b.co.jp>
2017-10-16 02:32:49 -0700

    ドリル7問目

---
+++ phpdrill1-7.php
@@ -0,0 +1,28 @@
+<?php
+//$num = 0;
+//if ($num == o){
+//  echo "it's zero!";
+//}
+//else {
+ // echo "it's not zero!";
+//}
+
+//$num = 45;
+//if ($num < 20){
+//  echo "less that 20!" . "\n";
+//}
+//elseif($num < 40){
+//  echo "less than 40!" . "\n";
+//}
+//else{
+//  echo "at least 40!" . "\n";
+//}
+
+$num = 11;
+if ($num > 10){
+  echo "more than 10 , then 1 substracted from num" . "\n";
+  $num -= 1;
+  echo $num . "\n";
+//echo $num - 1 . "\n";
+}
+?>
