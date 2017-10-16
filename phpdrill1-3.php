60fe7bc288bddfce4707b2d901857cd446030c62
tomohirohiratsuka <tomohiro.hiratsuka@plan-b.co.jp>
2017-10-15 22:52:45 -0700

    ドリル３問目

---
+++ phpdrill1-3.php
@@ -0,0 +1,9 @@
+<?php
+$ary = array(array("yuki"), 45, array("mana","kana"),array("name"=>"takashi"));
+print_r($ary[0]). "\n";
+echo $ary[0];
+print_r($ary[3]). "\n";
+print_r($ary[2][1]). "\n";
+
+
+?>
---
+++ vardumptest.php
@@ -0,0 +1,8 @@
+<?php
+$ary = array(array("yuki"), 45, array("mana","kana"),array("name"=>"takashi"));
+var_dump($ary[0]). "\n";
+var_dump($ary[3]). "\n";
+var_dump($ary[2][1]). "\n";
+
+
+?>
