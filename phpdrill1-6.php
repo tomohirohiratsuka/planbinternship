576ae9f6dc5ba365880a4e00be135c0fe1a75105
tomohirohiratsuka <tomohiro.hiratsuka@plan-b.co.jp>
2017-10-16 02:06:00 -0700

    ドリル6問目

--- phpdrill1-6.php
+++ phpdrill1-6.php
@@ -4,8 +4,14 @@ $ary2 = array(5,6,7,8,9);
 $ary3 = array();
 foreach ($ary2 as $value2){
 $value2 = $value2 + 10 ;
-echo $value2 . "\n";
+array_push($ary3, $value2);
+//$ary3[] = $value2;
 }
+foreach($ary3 as $value2){
+  echo $value2 . "\n";
+}
+echo "=====================\n";
+var_dump($ary3);
 //foreach ($ary as $value) {
 //echo $value . "\n";
 //}
