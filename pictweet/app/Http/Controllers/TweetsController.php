0f69fbd6744fec400da749b46d9ff14a56a68ee3
tomohirohiratsuka <tomohiro.hiratsuka@plan-b.co.jp>
2017-10-25 18:25:30 +0900

    laravel

---
+++ pictweet/app/Http/Controllers/TweetsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Tweet;
+
+class TweetsController extends Controller
+{
+    public function index(){
+      $tweets =Tweet::all();
+
+      return view('tweets.index')->with('tweets',$tweets);
+    }
+    public function create(){
+      return view('tweets.create');
+    }
+    public function store(Request $request)
+    {
+      Tweet::create(
+       array(
+        'name' => $request->name,
+        'image' => $request->image,
+        'text' => $request->text
+       )
+     );
+      return view('tweets.store');
+    }
+}
